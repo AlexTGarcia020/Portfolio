f19f8bc4b92874592027062850a79d72d60bf36a
AlexTGarcia020 <129820523+AlexTGarcia020@users.noreply.github.com>
2024-01-03 15:07:09 -0300

    V1.0.1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,55 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "@react-spring/web";
+import { animated } from "@react-spring/web";
 import "../CSS/Navbar.css";
+import { Icon } from "@iconify/react";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const menuAnimation = useSpring({
-    transform: isMenuOpen ? "scale(1)" : "scale(0)", 
-  });
-
+  
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <nav className="navbar">
+        <div className="topNav">
+        </div>
+            <ul className="navbar-icons">
+                <animated.li>
+                <div className="mt-3">
+                    <div className="icon-box d-flex gap-2">
+                        <div className="iconContent">
+                            <Icon icon="material-symbols:home-app-logo" className="fs-3" />
+                        </div>
+                        <div className="iconContent">
+                            <Icon icon="material-symbols:code" className="fs-3"/>
+                        </div>
+                        <div className="iconContent">
+                            <Icon icon="material-symbols:person-4-outline-rounded" className="fs-3" />
+                        </div>
+                            <hr className="vertical-hr me-1" />
+                        <div className="iconContentII">
+                            <Icon icon="akar-icons:linkedinv2-fill" className="fs-3" />
+                        </div>
+                        <div className="iconContentII">
+                            <Icon icon="uil:github-alt" className="fs-3" />
+                        </div>    
+                        <div className="iconContentII">
+                            <Icon icon="mdi:whatsapp" className="fs-3" />
+                        </div>
+                        <div className="ms-5">
+                            <img src="/Logo.svg" alt="logo-alex-nordic" className="img-fluid" />
+                        </div>
+                    </div>        
+                </div>
+                </animated.li>
+            </ul>
       <div className="mobile-menu-toggle" onClick={toggleMenu}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-
-     
-      <ul className="navbar-icons">
-        <animated.li style={menuAnimation}>
-          <div className="icon-box">
-            <i className="fa fa-home"></i>
-          </div>
-        </animated.li>
-      </ul>
     </nav>
   );
 }

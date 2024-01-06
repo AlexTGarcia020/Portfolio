9e4253668d3c5501a2847eb4c3ffc2847ce5cc8c
AlexTGarcia020 <129820523+AlexTGarcia020@users.noreply.github.com>
2024-01-06 04:00:52 -0300

    V.1.0.12

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -3,50 +3,50 @@ import "../CSS/Proyectos.css";
 
 function Proyectos() {
   return (
-    <div className="row">
+    <div className="row mt-5 g-0">
      <div className="col-md-4">
-        <figure className="d-flex flex-column">
-            <img src="/Logo.svg" className="w-50" alt="Descripción de la imagen" />
+        <figure className="d-flex flex-column p-2 justify-content-center">
+            <img src="/Color.png" className="w-50 rounded shadow" alt="Descripción de la imagen" />
             <figcaption>
             Descripción o título de la imagen o figura.
             </figcaption>
         </figure>
       </div>
       <div className="col-md-4">
-        <figure className="d-flex flex-column">
-            <img src="/Logo.svg" className="w-50" alt="Descripción de la imagen" />
+        <figure className="d-flex flex-column p-2 justify-content-center">
+            <img src="/Martini.png" className="w-50 rounded shadow" alt="Descripción de la imagen" />
             <figcaption>
             Descripción o título de la imagen o figura.
             </figcaption>
         </figure>
       </div>
       <div className="col-md-4">
-        <figure className="d-flex flex-column">
-            <img src="/Logo.svg" className="w-50" alt="Descripción de la imagen" />
+        <figure className="d-inline flex-column p-2 justify-content-center">
+            <img src="/Martei.png" className="w-50 rounded shadow" alt="Descripción de la imagen" />
             <figcaption>
             Descripción o título de la imagen o figura.
             </figcaption>
         </figure>
       </div>
       <div className="col-md-4">
-        <figure className="d-flex flex-column">
-            <img src="/Logo.svg" className="w-50" alt="Descripción de la imagen" />
+        <figure className="d-flex flex-column  p-2 justify-content-center">
+            <img src="/Jardim.png" className="w-50 rounded shadow" alt="Descripción de la imagen" />
             <figcaption>
             Descripción o título de la imagen o figura.
             </figcaption>
         </figure>
      </div>
      <div className="col-md-4">
-      <figure className="d-flex flex-column">
-        <img src="/Logo.svg" className="w-50" alt="Descripción de la imagen" />
+      <figure className="d-flex flex-column p-2  justify-content-center">
+        <img src="/Twitter.png" className="w-50 rounded shadow" alt="Descripción de la imagen" />
         <figcaption>
           Descripción o título de la imagen o figura.
         </figcaption>
       </figure>
       </div>
     <div className="col-md-4">
-      <figure className="d-flex flex-column">
-        <img src="/Logo.svg" className="w-50" alt="Descripción de la imagen" />
+      <figure className="d-flex flex-column p-2 justify-content-center">
+        <img src="/Hackflix.png" className="w-50 rounded shadow" alt="Descripción de la imagen" />
         <figcaption>
           Descripción o título de la imagen o figura.
         </figcaption>

fb9f0e9eaaa6224063cd63bd296f895604704d87
AlexTGarcia020 <129820523+AlexTGarcia020@users.noreply.github.com>
2024-01-07 16:29:35 -0300

    V.5

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function App() {
       </MediaQuery>
       <MediaQuery maxDeviceWidth={767}>
       <Navbar /> 
-      <div className="app-content" style={{height:'620vh'}}>
+      <div className="app-content" style={{height:'619vh'}}>
         <animated.div
           className="parallax-effect"
           style={{
